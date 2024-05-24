c7b11cb7434ee089cc02d6d12bde68f0135ce69c
AlfinFF16 <alfin160103@gmail.com>
2024-05-24 08:51:18 +0700

    update -- near final code

---
+++ shell_script/navigation.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Launch the robot
+source /opt/ros/noetic/setup.bash 
+source /home/jetson/ancabot_ws/devel/setup.bash
+
+# Running Node Index
+NAVIGATION_MODE=1
+
+# Start the navigation launch file in the background
+roslaunch hexapod_bringup navigation_$NAVIGATION_MODE.launch &
---
+++ start_button/scripts/start_button.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rospy
+import subprocess
+import RPi.GPIO as GPIO
+from std_msgs.msg import Bool
+
+# Define GPIO pin for the push button
+BUTTON_PIN = 11
+
+# Initialize GPIO
+GPIO.setmode(GPIO.BOARD)                                      # BOARD pin-numbering scheme
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)     # button pin set as input
+
+
+def button_callback(channel):
+    rospy.loginfo("Button pressed!")
+    # Launch your ROS launch file here
+    subprocess.call("/home/jetson/ancabot_ws/src/shell_script/navigation.sh", shell=True)
+    # Publish a message to the 'button_pressed' topic
+    pub.publish(Bool(True))
+
+# Initialize the ROS node
+rospy.init_node('button_controller_node')
+
+# Set up the publisher for the button press
+pub = rospy.Publisher('button_pressed', Bool, queue_size=10)
+
+# Subscribe to button topic
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=300)
+
+rospy.loginfo("Button controller node ready...")
+
+rospy.spin()
+
+# Clean up GPIO
+GPIO.cleanup()
+
