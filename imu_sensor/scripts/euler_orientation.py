0c203432fdd7ad2601a86970993d60ddc60f839d
AlfinFF16 <alfin160103@gmail.com>
2024-05-27 11:49:31 +0700

    update euler subscriber

--- imu_sensor/scripts/euler_orientation.py
+++ imu_sensor/scripts/euler_orientation.py
@@ -11,8 +11,14 @@ class ImuToEulerConverter:
         rospy.init_node('imu_to_euler_converter_node')
         self.imu_sub = rospy.Subscriber('/imu/data', Imu, self.imu_callback)
         self.euler_pub = rospy.Publisher('/euler_topic', PoseStamped, queue_size=10)
+        self.initial_orientation = None
 
     def imu_callback(self, msg):
+        if self.initial_orientation is None:
+            # Store the initial orientation
+            quat = (msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
+            self.initial_orientation = euler_from_quaternion(quat)
+
         # Convert quaternion to Euler angles
         quat = (msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
         roll, pitch, yaw = euler_from_quaternion(quat)
@@ -22,15 +28,20 @@ class ImuToEulerConverter:
         pitch_deg = math.degrees(pitch)
         yaw_deg = math.degrees(yaw)
 
+        # Calculate angular displacement relative to initial orientation
+        roll_displacement = roll_deg - math.degrees(self.initial_orientation[0])
+        pitch_displacement = pitch_deg - math.degrees(self.initial_orientation[1])
+        yaw_displacement = yaw_deg - math.degrees(self.initial_orientation[2])
+
         # Print Euler angles in degrees
-        rospy.loginfo("Roll: {:.2f} deg, Pitch: {:.2f} deg, Yaw: {:.2f} deg".format(roll_deg, pitch_deg, yaw_deg))
+        rospy.loginfo("Roll: {:.2f} deg, Pitch: {:.2f} deg, Yaw: {:.2f} deg".format(roll_displacement, pitch_displacement, yaw_displacement))
 
         # Create Euler angle message
         euler_msg = PoseStamped()
         euler_msg.header = msg.header
-        euler_msg.pose.position.x = roll_deg
-        euler_msg.pose.position.y = pitch_deg
-        euler_msg.pose.position.z = yaw_deg
+        euler_msg.pose.position.x = roll_displacement
+        euler_msg.pose.position.y = pitch_displacement
+        euler_msg.pose.position.z = yaw_displacement
 
         # Publish Euler angle message
         self.euler_pub.publish(euler_msg)
